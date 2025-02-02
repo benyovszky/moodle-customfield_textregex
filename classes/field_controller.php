b7ed0279145821807b3899d41da96a7bbb1e47ae
benyovszky <molbence@gmail.com>
2025-02-02 18:18:24 +0100

    Test default value only if it is not empty

--- classes/field_controller.php
+++ classes/field_controller.php
@@ -84,7 +84,7 @@ class field_controller extends \core_customfield\field_controller {
 
         if (preg_match($data['configdata']['regex'], '') === false) {
             $errors['configdata[regex]'] = get_string('errorconfigregex', 'customfield_textregex');
-        } else if (!preg_match($data['configdata']['regex'], $data['configdata']['defaultvalue'])) {
+        } else if (!empty($data['configdata']['defaultvalue']) && !preg_match($data['configdata']['regex'], $data['configdata']['defaultvalue'])) {
             $errors['configdata[defaultvalue]'] = get_string('errorconfigdefault', 'customfield_textregex');
         }
 
